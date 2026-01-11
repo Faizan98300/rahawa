8aa8284589236f0e8d7adfa9ce6abf1a80f87bdd
Faizan98300 <gando4189@gmail.com>
2026-01-12 07:17:18 +0800

    Remove favicon link from header
    Removed the favicon link from the header component.

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,6 @@ export function Header() {
   const { cartCount } = useCart()
   const [currentUser, setCurrentUser] = useState<UserType | null>(null)
   const router = useRouter()
-  <link rel="icon" href="111.png" type="image/x-icon">
 
   useEffect(() => {
     setCurrentUser(getCurrentUser())
